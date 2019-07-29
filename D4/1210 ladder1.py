e91da1fdff978f086d8f875fc2f51fc77a0eca70
unsung107 <unsung102@naver.com>
2019-07-29 19:57:17 +0900

    20190729

---
+++ D4/1210 ladder1.py
@@ -0,0 +1,36 @@
+near = [(0,-1), (0,1), (-1,0)]
+def ladder_orgi(y,x,ladder):
+
+    for dy,dx in near:
+        if y + dy == 0:
+            return x + dx
+    
+    if x == 99:
+        for dy,dx in [(0,-1), (-1,0)]:
+            if ladder[y+dy][x+dx] == 1:
+                ladder[y][x] = 3
+                return ladder_orgi(y+dy, x+dx,ladder)
+    
+    if x == 0:
+        for dy,dx in [(0,1), (-1,0)]:
+            if ladder[y+dy][x+dx] == 1:
+                ladder[y][x] = 3
+                return ladder_orgi(y+dy, x+dx,ladder)
+
+    for dy,dx in near:
+        if ladder[y+dy][x+dx] == 1:
+            ladder[y][x] = 3
+            return ladder_orgi(y+dy, x+dx,ladder)
+
+
+
+for rounds in range(1,11):
+    ladder = []
+    ro = int(input())
+    for i in range(100):
+        ladder.append(list(map(int,input().split())))
+    
+    for i in range(100):
+        if ladder[99][i] == 2:
+            exit_index = i
+    print(f'#{rounds} {ladder_orgi(99,exit_index,ladder)}')
--- D4/1226 미로.py
+++ D4/1226 미로.py
@@ -1,3 +1,25 @@
+near = [(-1,0), (0,1), (1,0), (0,-1)]
+def maze_adventure(y, x, maze):
+    
+
+    for dy,dx in near:
+        if maze[y+dy][x+dx] == 3:
+            return 1
+    
+    for dy,dx in near:
+        if maze[y+dy][x+dx] == 0:
+            maze[y+dy][x+dx] = 4
+            return maze_adventure(y+dy,x+dx,maze)
+    
+    for i in range(4,9):
+        for dy,dx in near:
+            if maze[y+dy][x+dx] == i:
+                maze[y+dy][x+dx] += 1
+                return maze_adventure(y+dy,x+dx,maze)
+    
+    return 0
+        
+
 from pprint import pprint
 for rounds in range(1,11):
     ro = int(input())
@@ -6,47 +28,6 @@ for rounds in range(1,11):
         maze.append(list(map(int,input())))
 
     start_index = [(y,x) for y in range(16) for x in range(16) if maze[y][x] == 2][0]
-    end_index = [(y,x) for y in range(16) for x in range(16) if maze[y][x] == 3][0]
+
+    print(f'#{rounds} {maze_adventure(start_index[0], start_index[1],maze)}')
     
-    y, x = start_index[0], start_index[1]
-    
-    path = []
-    cross_section = (y, x)
-    near = [(-1,0), (1,0), (0,-1), (0,1)]
-    temp_path = []
-
-    
-    
-    while True:
-
-        maze[y][x] = 4
-        near_path = [(i,l) for (i,l) in near if maze[y+i][x+l] == 0]
-        
-        if near_path.count(0) == 1:
-            y += i; x += l
-            path.append((y,x))
-        
-        elif near_path.count(0) > 1:
-            cross_section = (y, x)
-            
-            for cross_num in range(near_path.count(0)):
-                if maze[y+near_path[cross_num][0]][x+near_path[cross_num][1]] == 0:
-                    y += near_path[cross_num[0]]; x += near_path[cross_num][1]
-            path.append((y,x))
-        
-        elif near_path.count(0) == 0:
-            path.append((y,x))
-            y, x = cross_section
-
-
-
-
-
-        
-
-        
-
-
-
-
-
