d052ff198aa8aeba54d5e82f1d9fe5780e19a08f
unsung107 <unsung102@naver.com>
2019-08-06 17:51:40 +0900

    20190806

--- D2/5020 색칠하기.py
+++ D2/5020 색칠하기.py
@@ -2,15 +2,15 @@ result = []
 for round in range(int(input())):
     board = [[0 for _ in range(10)] for _ in range(10)]
     N = int(input())
-    count = 0
-    for color in range(N):
-        r1, c1, r2, c2, color = map(int,input().split())
+    count = 0 #카운트 초기화
+    for color in range(N): 
+        r1, c1, r2, c2, color = map(int,input().split()) #정보를 받아온다
         for y in range(r1, r2 + 1):
             for x in range(c1, c2 + 1):
-                if board[y][x] == 0:
+                if board[y][x] == 0: #보드가 0이면 색칠
                     board[y][x] = color
-                elif board[y][x] != color and board[y][x] != 3:
-                    board[y][x] = 3
+                elif board[y][x] != color and board[y][x] != 3: #색깔이 다르고 3이아니라면 
+                    board[y][x] = 3# 보라색 색칠후 카운트 +1
                     count += 1
         
     result.append('#%d %d' %(round + 1, count))
--- D3/1244 최대상금.py
+++ D3/1244 최대상금.py
@@ -1,13 +1,39 @@
-import sys
-sys.stdin = open('input.txt', 'r')
+def max_prices(depth, number):
+    if depth == K:
+        return number
 
-result = []
+    if cache[depth].get(number) != None:
+        res = cache[depth][number]
+        return res
+
+    res = 0
+    for [x,y] in change_cases:
+        temp = changelist(list(number), x, y)
+        res = max(res, int(max_prices(depth + 1, temp)))
+    
+    cache[depth][number] = res
+    return res
+
+def changelist(arr, c1, c2):
+    arr[c1], arr[c2] = arr[c2], arr[c1]
+    arr = ''.join(arr)
+    return arr
+
 for round in range(int(input())):
     number, K = input().split()
     K = int(K)
-
-    numbers = {}
-    for i in range(len(number)):
-        numbers[i] = int(number[i])
+    length = len(number)
     
-    print(numbers)
+    digits = list(range(length)) #자리수.
+    change_cases = [] #자리바꿀수있는경우의수
+    for i in range(1 << length):
+        bubun = []
+        for j in range(length):
+            if i & (1 << j):
+                bubun.append(digits[j])
+        if len(bubun) == 2:
+            change_cases.append(bubun)
+    
+    cache = [{} for _ in range(K + 1)]
+
+    print(f'#{round + 1} {max_prices(0,number)}')
