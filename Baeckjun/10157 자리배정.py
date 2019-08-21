e1d3fcc08bae8e9ccf4e047ee74a5f270f07ea76
unsung107 <unsung102@naver.com>
2019-08-21 12:08:38 +0900

    20190821

--- Baeckjun/10157 자리배정.py
+++ Baeckjun/10157 자리배정.py
@@ -9,7 +9,7 @@ elif K == 1:
     print(1, 1)
 
 else:
-    root = ((h + w) ** 2 - 4 * K) ** 0.5
+    root = ((h + w) ** 2 - 4 * (K-1)) ** 0.5
 
     n = int(((h + w) - root) // 4)
 
@@ -23,49 +23,67 @@ else:
     curve = 0
     
     while True:
-        if curve % 4 == 0:
-            for i in range(max_y - min_y + 1):
-                now += 1
-                if now == K:
-                    break
-                y += 1
-        if now != K:
-            x += 1
         if now == K:
             print(x, y)
-
-        if curve % 4 == 1:
-            for i in range(max_x - min_x + 1):
+            break
+            
+        if curve % 4 == 0:
+            while min_y <= y < max_y:
+                y += 1
                 now += 1
                 if now == K:
                     break
-                x += 1
+                
         if now != K:
-            y -= 1
+            curve += 1
         if now == K:
+            # y -= 1
+            print(x, y)
+            break
+
+        if curve % 4 == 1:
+            while min_x <= x < max_x:
+                x += 1
+                now += 1
+                if now == K:
+                    break
+                
+        if now != K:
+            # y -= 1
+            curve += 1
+        if now == K:
+            # x -= 1
             print(x, y)
             break
 
         if curve % 4 == 2:
-            for i in range(max_y - min_y + 1):
+            while min_y < y <= max_y:
+                y -= 1
                 now += 1
                 if now == K:
                     break
-                y -= 1
+                
         if now != K:
-            x -= 1
+            # x -= 1
+            curve += 1
         if now == K:
+            # y += 1
             print(x, y)
+            break
 
         if curve % 4 == 3:
-            for i in range(max_x - min_x + 1):
+            while min_x < x <= max_x:
+                x -= 1
                 now += 1
                 if now == K:
                     break
-                x -= 1
+                
         if now != K:
-            y += 1
+            # y += 1
+            curve += 1
         if now == K:
+            # x += 1
             print(x, y)
+            break
 
 
