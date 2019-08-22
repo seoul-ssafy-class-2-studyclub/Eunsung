e8d8915ccea22443d45ae57b6c36d0b32381e141
unsung107 <unsung102@naver.com>
2019-08-22 18:54:40 +0900

    20190822

--- Baeckjun/2643 색종이 올려쌓기.py
+++ Baeckjun/2643 색종이 올려쌓기.py
@@ -7,4 +7,16 @@ for _ in range(N):
         w, h = h, w
     
     papers.append((w, h))
+papers.sort(key=lambda x: x[0], reverse=True)
+count = [1 for _ in range(N)]
 print(papers)
+for i in range(1, N):
+    temp = 1
+    for j in range(i - 1):
+        if papers[i][1] >= papers[j][1]:
+            if count[j] > temp:
+                temp = count[j]
+        count[i] = temp + 1
+print(count)
+    
+
