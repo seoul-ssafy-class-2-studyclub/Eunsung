893937cd5d098f298500ed788f66e3d8776fde86
eunsung kim <unsung107@gmail.com>
2020-09-07 21:49:13 +0900

    20200907

--- kakao_20200906/03_자물쇠와열쇠.py
+++ kakao_20200906/03_자물쇠와열쇠.py
@@ -1,6 +1,56 @@
-def solution(key, lock):
-    answer = True
-    return answer
+from pprint import pprint
 
+def solution(key, lock):
+    m = len(key)
+    n = len(lock)
+    LENGTH_OF_BACKGROUND = n + 2 * m - 2
+
+    background = [[0] * (LENGTH_OF_BACKGROUND) for _ in range(LENGTH_OF_BACKGROUND)]
+
+    for dy in range(n):
+        for dx in range(n):
+            background[m - 1 + dy][m - 1 + dx] = lock[dy][dx]
+
+    def is_opened(tmp_background):
+
+        for dy in range(n):
+            for dx in range(n):
+                if tmp_background[m - 1 + dy][m - 1 + dx] != 1:
+                    return False
+        return True
+    
+    def copy_lock(background):
+        copied_background = []
+
+        for i in range(LENGTH_OF_BACKGROUND):
+            copied_background.append(background[i][:])
+
+        return copied_background
+    
+    def check_key(key, background):
+
+        for start_y in range(n + m - 1):
+            for start_x in range(n + m - 1):
+
+                tmp_background = copy_lock(background)
+
+                for dy in range(m):
+                    for dx in range(m):
+                        tmp_background[start_y + dy][start_x + dx] += key[dy][dx]
+                if is_opened(tmp_background): return True
+        return False
+    
+    def rotate(key):
+        tmp_key = [[0] * m for _ in range(m)]
+        for y in range(m):
+            for x in range(m):
+                tmp_key[x][m - y - 1] = key[y][x]
+        return tmp_key
+
+    for _ in range(4):
+        key = rotate(key)
+        if check_key(key, background): return True
+
+    return False
 
 print(solution([[0, 0, 0], [1, 0, 0], [0, 1, 1]], [[1, 1, 1], [1, 1, 0], [1, 0, 1]]	))
